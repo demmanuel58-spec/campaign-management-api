8cf27a50317adc29cbdb5f40efa5e17e1783337a
demmanuel58-spec <demmanuel58@gmail.com>
2026-08-29 22:08:04 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -28,10 +28,10 @@ app.add_middleware(
 )
 
 @app.middleware("http")
-def add_request_id_middleware(request: Request, call_next):
+async def add_request_id_middleware(request: Request, call_next):
     request_id = request.headers.get("X-Request-ID", str(uuid.uuid4()))
     request.state.request_id = request_id
-    response = call_next(request)
+    response = await call_next(request)
     response.headers["X-Request-ID"] = request_id
     return response
 
